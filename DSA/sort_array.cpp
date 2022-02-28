3354388dcd9652f7ed42b8d72335090933d0bd25
Surya Prakash Gupta <66009201+Anmol-Gup@users.noreply.github.com>
2022-02-28 19:30:47 +0530

    Update sort_array.cpp

--- DSA/sort_array.cpp
+++ DSA/sort_array.cpp
@@ -1,14 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int arr[] = {1, 1, 0, 2, 0, 1, 2};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    // Approach-1
-    sort(arr, arr+n);
-    
-   // Approach-2
-  void sorting012Array(int arr[], int n) {
+// Approach-2
+void swap(int *x, int *y) 
+{
+  int temp = *x;
+  *x = *y;
+  *y = temp;
+}
+void sorting012Array(int a[], int n) {
+  int i = 0, j = 0, k = n - 1;
+  while (j <= k) {
+    if (a[j] == 0) {
+      swap( &a[j], &a[i]);
+      i++;
+      j++;
+    } else if (a[j] == 1)
+      j++;
+    else if (a[j] == 2) {
+      swap( &a[j], &a[k]);
+      k--;
+    }
+  }
+}
+
+// Approach-3
+void sorting012Array(int arr[], int n) {
     int count0 = 0, count1 = 0, count2 = 0;
     for(int i=0; i<n; i++) {
         if (arr[i] == 0)
@@ -26,7 +43,12 @@ int main() {
     while(count2--)
         arr[i++] = 2;
     }
-
+}
+int main() {
+    int arr[] = {1, 1, 0, 2, 0, 1, 2};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    // Approach-1
+    sort(arr, arr+n);
     for (int i=0; i<n; i++)
         cout << arr[i] << " ";
 
